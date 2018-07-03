bdfa22727aea5cacd912591e7f6257322bbf0137
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-03 11:06:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,13 +11,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
-  cheese_types.each do |cheese|
-    if array.include?(cheese) == true
-      return cheese
-    end   
-  end
+  cheese_types.each { |cheese| if array.include?(cheese) == true 
+  return cheese end }  
   nil
 end
